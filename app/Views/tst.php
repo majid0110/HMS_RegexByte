ce7ebb6858e43abb6cd2b8566bb134d75a01b1b8
Majid khan <81530072+majid0110@users.noreply.github.com>
2024-06-21 00:28:16 +0500

    Update tst.php

--- app/Views/tst.php
+++ app/Views/tst.php
@@ -1,28 +1,6 @@
 <?Php 
 echo('This is a test massage');
 echo ('This is test 2 Massage');
-echo ('This is test 3 Massage');
-echo ('This is test 4 Massage');
-echo ('This is test 5 Massage');
-echo ('This is test 6 Massage');
-echo ('This is test 7 Massage');
-echo ('This is test 8 Massage');
-echo ('This is test 9 Massage');
-echo ('This is test 10 Massage');
-echo ('This is test 11 Massage');
-echo ('This is test 12 Massage');
-echo ('This is test 13 Massage');
-echo ('This is test 14 Massage');
-echo ('This is test 15 Massage');
-echo ('This is test 16 Massage');
-echo ('This is test 17 Massage');
-echo ('This is test 18 Massage');
-echo ('This is test 19 Massage');
-echo ('This is test 20 Massage');
-echo ('This is test 21 Massage');
-echo ('This is test 22 Massage');
-echo ('This is test 23 Massage');
-echo ('This is test 24 Massage');
-echo ('This is test 25 Massage');
+
 
 ?>
