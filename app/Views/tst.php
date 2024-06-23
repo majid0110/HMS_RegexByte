1db33499ee4c5f1f792d74e9f42ac0851da6f0fd
Majid khan <81530072+majid0110@users.noreply.github.com>
2024-06-23 18:32:49 +0500

    Update tst.php

--- app/Views/tst.php
+++ app/Views/tst.php
@@ -2,10 +2,5 @@
 echo('This is a test massage');
 echo ('This is test 2 Massage');
 echo ('This is test 3 Massage');
-echo ('This is test 4 Massage');
-echo ('This is test 5 Massage');
-echo ('This is test 6 Massage');
-echo ('This is test 7 Massage');
-echo ('This is test 8 Massage');
-echo ('This is test 9 Massage');
+
 ?>
